0278cd25d584002c0856f26b8cd10cb6ce8b9eb9
Maximilian Stölzle <maximilian@stoelzle.ch>
2019-10-11 10:03:21 +0100

    finish processor

--- packages/bag_processor.py
+++ packages/bag_processor.py
@@ -1,6 +1,6 @@
 import rosbag
 
-from opencv import cv2
+import cv2
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
@@ -8,7 +8,7 @@ from pprint import pprint
 
 bridge = CvBridge()
 
-filename = 'example_rosbag_H3'
+filename = '2019-10-09-12-09-35'
 read_bag = rosbag.Bag('/data/'+filename+'.bag')
 write_bag = rosbag.Bag('/data/'+filename+'_annotated.bag','w')
 
@@ -18,33 +18,38 @@ try:
         i = i+1
         unix_timestamp = t.to_sec()
 
-        pprint(msg)
+        if msg._type == 'sensor_msgs/CompressedImage':
+            try:
+                img = bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+            except CvBridgeError as e:
+                print(e)
 
-        try:
-            img = bridge.imgmsg_to_cv2(msg, "bgr8")
-        except CvBridgeError as e:
-            print(e)
+            h = img.shape[0]
+            w = img.shape[1]
 
-        h, w = np.shape(img)
+            # org
+            org = (int(np.floor(1/8.0*w)),int(np.floor(h/8.0)))
 
-        # org
-        org = (np.floor(h/4), np.floor(3/8*w))
+            # font
+            fontFace = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 0.8
 
-        # font
-        fontFace = cv2.FONT_HERSHEY_SIMPLEX
-        fontScale = 1
+            # Blue color in BGR
+            color = (255, 0, 0)
 
-        # Blue color in BGR
-        color = (255, 0, 0)
+            cv2.putText(img, str(unix_timestamp), org, fontFace, fontScale, color)
 
-        cv2.putText(img, str(unix_timestamp), org, fontFace, fontScale, color)
+            if i == 0:
+                cv2.imwrite('/data/'+filename+'_0.png', img)
 
-        if i == 0:
-            cv2.imshow('Annotated image', img)
+            try:
+                new_msg = bridge.cv2_to_compressed_imgmsg(img, "jpg")
+            except CvBridgeError as e:
+                print(e)
 
-        new_msg = bridge.cv2_to_imgmsg(img, "bgr8")
+            #new_msg.header = msg.header
 
-        write_bag.write(topic, t, new_msg)
+            write_bag.write(topic, new_msg, t)
 
 finally:
     read_bag.close()
